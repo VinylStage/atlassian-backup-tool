ecb96d0705489570cf0636c51bcc368f52a24fc0
Vinyl Stage <mcpe9869@gmail.com>
2025-12-04 10:05:13 +0900

    WIP: get data test

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 # main.py
+import json
 
-def hello():
-    print("hello world")
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import dotenv_values
 
-if __name__ == "__main__":
-    hello()
+
+# def hello():
+#     print("hello world")
+
+
+# if __name__ == "__main__":
+#     hello()
+
+config = dotenv_values(".env")
+# print(config)
+ID = 184287513
+url = f"https://{config['DOMAIN']}/wiki/api/v2/folders/{ID}"
+
+auth = HTTPBasicAuth(config["EMAIL"], config["API_TOKEN"])
+
+headers = {"Accept": "application/json"}
+
+params = {"include-direct-children": "true"}
+
+response = requests.request(
+    "GET", url, headers=headers, auth=auth, params=params, timeout=1000
+)
+
+print(
+    json.dumps(
+        json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")
+    )
+)
